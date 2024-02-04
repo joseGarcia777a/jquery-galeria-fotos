140cdf98d3df2f01b474bb7794ea2d12f0d41c11
Jose Garcia <josegarcia@MacBook-Pro-de-Jose.local>
2024-02-04 10:42:39 -0300

    commit ninicial

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $('header button').click(function(){
+        $('form').slideDown();
+    });
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    });
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        
+        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imgan-link">
+                <a href="${enderecoDaNovaImagem}" title="Ver imagem em tamanho real" target="_blank">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).fadeIn();
+        $(novoItem).appendTo('ul');
+        $('#endereco-imagem-nova').val(' ');
+    });
+
+});
+
